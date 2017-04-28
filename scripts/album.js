bacee6dd0cd20b1a644f36f986fdd2d1fdd57439
rami hamdan <zrnhamdan@gmail.com>
2017-04-28 19:05:32 -0400

    updated checkpoint 11 assignment

--- scripts/album.js
+++ scripts/album.js
@@ -53,23 +53,23 @@ var createSongRow = function(songNumber, songName, songLength){
   ;
 
   return template;
-}
+};
 
 var setCurrentAlbum = function(album){
-  var albumTitle = document.getElementsByClassName('album-view-title')[0];
-  var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-  var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-  var albumImage = document.getElementsByClassName('album-cover-art')[0];
-  var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+    var albumTitle = document.getElementsByClassName('album-view-title')[0];
+    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+    var albumImage = document.getElementsByClassName('album-cover-art')[0];
+    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-  albumTitle.firstChild.nodeValue = album.title;
-  albumArtist.firstChild.nodeValue = album.artist;
-  albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-  albumImage.setAttribute('src', album.albumArtUrl);
+    albumTitle.firstChild.nodeValue = album.title;
+    albumArtist.firstChild.nodeValue = album.artist;
+    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+    albumImage.setAttribute('src', album.albumArtUrl);
 
-  albumSongList.innerHTML = ' ';
+    albumSongList.innerHTML = ' ';
 
-  for ( var i = 0; i < album.songs.length; i++ ) {
+    for ( var i = 0; i < album.songs.length; i++ ) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
   }
 
@@ -77,24 +77,31 @@ var setCurrentAlbum = function(album){
 
 window.onload = function(){
     setCurrentAlbum(albumPicasso);
+    //
+    // var index = 1;
+    // albumImage.addEventListener("click", function(event){
+    //     setCurrentAlbum(albums[index]);
+    //     index++;
+    //     if (index == albums.length) {
+    //       index = 0;
+    //     }
+    // });
 };
 
-//define event listener and specify function to return. needs to be setCurrentAlbum
-//setCurrentAlbum should loop through albums based on a count. for example, set a value for each album
-//if count is divisible by 2, should return same album. if divisible by 3, return next album, if neither, return other album.
-// count needs to start at 1 maybe?
-//define function that will be tied to event listener
+var albums = [albumPicasso, albumMarconi, otherAlbum];
+
 var count = 1;
 document.getElementsByClassName('album-cover-art')[0].addEventListener("click", function(){
-       count++;
-       if ( count% 3 === 0){
-         setCurrentAlbum(otherAlbum);
+
+       if ( count < albums.length-1 ){
+         setCurrentAlbum(albums[count]);
+         count++;
+         console.log(count);
        }
-       else if( count%2 ===0){
-         setCurrentAlbum(albumMarconi);
-       }
-       else {
-         setCurrentAlbum(albumPicasso);
+       else if( count === albums.length-1 ){
+         setCurrentAlbum(albums[count]);
+         count = 0;
+         console.log(count);
        }
 
 } );
