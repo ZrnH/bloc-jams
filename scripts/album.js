da1ca98ca11d924876371169c5d0ea67d4b2a00a
rami hamdan <zrnhamdan@gmail.com>
2017-04-29 12:20:42 -0400

    checkpoint 12 completed tasks but incorrect function

--- scripts/album.js
+++ scripts/album.js
@@ -60,6 +60,22 @@ var setCurrentAlbum = function(album){
 
 };
 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 window.onload = function(){
     setCurrentAlbum(albumPicasso);
+    // this doesn't work in firefox
+    songListContainer.addEventListener('mouseover', function(event) {
+        if (event.target.parentElement.className === 'album-view-song-item'){
+              event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+        }
+    });
+    for (var i=0; i<songRows.length; i++) {
+      songRows[i].addEventListener('mouseleave', function(event) {
+          this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+      });
+    }
+
 };
