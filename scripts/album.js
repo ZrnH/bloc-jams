1b27c8518996f58dcb68f4a477f0e061ce668d04
rami hamdan <zrnhamdan@gmail.com>
2017-05-21 15:17:54 -0400

    checkpoint 21 complete

--- scripts/album.js
+++ scripts/album.js
@@ -25,7 +25,14 @@ var getSongNumberCell = function(number){
         $(this).html(pauseButtonTemplate);
         setSong(songNumber);
         currentSoundFile.play();
+        updateSeekBarWhileSongPlays();
         updatePlayerBarSong();
+        currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+
+        var $volumeFill = $('.volume .fill');
+        var $volumeThumb = $('.volume .thumb');
+        $volumeFill.width(currentVolume + '%');
+        $volumeThumb.css({left: currentVolume + '%'});
 
       } else if (currentlyPlayingSongNumber === songNumber) {
         $(this).html(playButtonTemplate);
@@ -43,7 +50,10 @@ var getSongNumberCell = function(number){
           $(this).html(playButtonTemplate);
           $('.main-controls .play-pause').html(playerBarPlayButton);
         }
+
       }
+
+      
   };
 
   var onHover = function(event) {
@@ -93,6 +103,69 @@ var setCurrentAlbum = function(album){
 
 };
 
+var updateSeekBarWhileSongPlays = function(){
+    if (currentSoundFile){
+      currentSoundFile.bind('timeupdate',function(event){
+          var seekBarFillRatio = this.getTime() / this.getDuration();
+          var $seekBar = $('.seek-control .seek-bar');
+
+          updateSeekPercentage($seekBar, seekBarFillRatio);
+      });
+    }
+};
+
+var updateSeekPercentage = function ( $seekBar, seekBarFillRatio ) {
+    var offsetXPercent = seekBarFillRatio * 100;
+
+    offsetXPercent = Math.max(0, offsetXPercent);
+    offsetXPercent = Math.min(100, offsetXPercent);
+
+    var percentageString = offsetXPercent + '%';
+    $seekBar.find('.fill').width(percentageString);
+    $seekBar.find('.thumb').css({left: percentageString});
+};
+
+var setupSeekBars = function(){
+  var $seekBars = $('.player-bar .seek-bar');
+
+  $seekBars.click(function(event){
+      var offsetX = event.pageX = $(this).offset().left;
+      var barWidth = $(this).width();
+      var seekBarFillRatio = offsetX / barWidth;
+
+      if ($(this).parent().attr('class') == 'seek-control'){
+          seek(seekBarFillRatio * currentSoundFile.getDuration());
+      } else {
+          setVolume(seekBarFillRatio * 100);
+      }
+
+      updateSeekPercentage($(this), seekBarFillRatio);
+  });
+
+  $seekBars.find('.thumb').mousedown(function(event){
+      var $seekBar = $(this).parent();
+
+      $(document).bind('mousemove.thumb', function(event){
+          var offsetX = event.pageX - $seekBar.offset().left;
+          var barWidth = $seekBar.width();
+          var seekBarFillRatio = offsetX / barWidth;
+
+          if ($(this).parent().attr('class') == 'seek-control'){
+              seek(seekBarFillRatio * currentSoundFile.getDuration());
+          } else {
+              setVolume(seekBarFillRatio * 100);
+      }
+
+          updateSeekPercentage($seekBar, seekBarFillRatio);
+      });
+
+      $(document).bind('mouseup.thumb', function(){
+          $(document).unbind('mousemove.thumb');
+          $(document).unbind('mouseup.thumb');
+      });
+  });
+};
+
 var trackIndex = function(album, song){
     return album.songs.indexOf(song);
 };
@@ -110,6 +183,12 @@ var setSong = function(songNum){
 
     setVolume(currentVolume);
 };
+
+var seek = function(time) {
+  if (currentSoundFile){
+    currentSoundFile.setTime(time);
+  }
+}
 
 var setVolume = function(volume){
     if(currentSoundFile){
@@ -129,7 +208,7 @@ var nextSong = function(){
 
     setSong(currentSongIndex + 1);
     currentSoundFile.play();
-
+    updateSeekBarWhileSongPlays();
     updatePlayerBarSong();
 
     var $nextSongNumberCell =  getSongNumberCell(currentlyPlayingSongNumber);
@@ -151,7 +230,7 @@ var previousSong = function(){
 
     setSong(currentSongIndex + 1);
     currentSoundFile.play();
-
+    updateSeekBarWhileSongPlays();
     updatePlayerBarSong();
 
     $('.main-controls .play-pause').html(playerBarPauseButton);
@@ -166,7 +245,7 @@ var previousSong = function(){
 var updatePlayerBarSong = function(){
     // this makes sense, but not following where the title is being stored
     $('.currently-playing .song-name').text(currentSongFromAlbum.title);
-    $('.currently-playing . artistName').text(currentAlbum.artist);
+    $('.currently-playing .artistName').text(currentAlbum.artist);
     $('currently-playing .artist-song-mobile').text(currentSongFromAlbum.title +' - ' + currentAlbum.artist);
     $('.main-controls .play-pause').html(playerBarPauseButton);
 
@@ -203,5 +282,5 @@ $(document).ready(function(){
     setCurrentAlbum(albumPicasso);
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
-    // this doesn't work in firefox
+    setupSeekBars();
 });
